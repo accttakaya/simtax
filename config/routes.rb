5bae02e5c567954af2feae7a4225926ca6baea7c
accttakaya <takaya.satou.0206@gmail.com>
2020-02-21 21:39:18 +0900

    done(user,frontmarkup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root to: "functions#index"
+  devise_for :users
+  root "functions#index"
+  resources :users, only: [:edit, :update]
   post '/about/result1', to: 'about#result1'
   post '/about/result2', to: 'about#result2'
   resources :functions,only: [:show] do
     resources :about,only: [:show]
   end
+  resources :mypage,only: [:show]
+  
 
 end
